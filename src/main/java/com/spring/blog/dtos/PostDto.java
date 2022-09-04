3cb388bdb51bcd2e8852eb9d7d26a0322360c321
ptcjw <ptcjw201@gmail.com>
2022-09-04 09:57:50 +0900

    Dto, Exception

---
+++ src/main/java/com/spring/blog/dtos/PostDto.java
@@ -0,0 +1,13 @@
+package com.spring.blog.dtos;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
+
+
+}
