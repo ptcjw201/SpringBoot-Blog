e4b280d2842cd69d8e0709fad929c2c40dd3b0fc
ptcjw <ptcjw201@gmail.com>
2022-09-09 14:17:05 +0900

    admin/posts 화면구현

--- src/main/java/com/spring/blog/controller/PostController.java
+++ src/main/java/com/spring/blog/controller/PostController.java
@@ -1,7 +1,26 @@
 package com.spring.blog.controller;
 
+import com.spring.blog.dto.PostDto;
 import com.spring.blog.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@Controller
 public class PostController {
     private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/admin/posts")
+    public String posts(Model model){
+        List<PostDto> posts = postService.findAllPosts();
+        model.addAttribute("posts", posts);
+        System.out.println("herer");
+        return "/admin/posts";
+    }
 }
