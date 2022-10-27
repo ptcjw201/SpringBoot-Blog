225869482ed1bd5ffc44442561b6fcb110afc9f3
ptcjw <ptcjw201@gmail.com>
2022-10-27 13:38:19 +0900

    client view 추가

--- src/main/java/com/spring/blog/controller/BlogController.java
+++ src/main/java/com/spring/blog/controller/BlogController.java
@@ -1,4 +1,35 @@
 package com.spring.blog.controller;
 
+
+import com.spring.blog.dto.PostDto;
+import com.spring.blog.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
 public class BlogController {
+
+    private PostService postService;
+    public BlogController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/")
+    public String viewBlogPosts(Model model) {
+        List<PostDto> postsResponse = postService.findAllPosts();
+        model.addAttribute("postsResponse", postsResponse);
+        return "/blog/view_posts";
+    }
+
+    @GetMapping("/post/{postUrl}")
+    public String showPost(@PathVariable("postUrl") String postUrl,
+                           Model model){
+        PostDto post = postService.findPostByUrl(postUrl);
+        model.addAttribute("post", post);
+        return "blog/blog_post";
+    }
 }
