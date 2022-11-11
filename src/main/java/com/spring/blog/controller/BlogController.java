8ac0e92ebaa2c0bc642801687ce603488a77097f
ptcjw <ptcjw201@gmail.com>
2022-11-11 12:03:55 +0900

    user search 추가

--- src/main/java/com/spring/blog/controller/BlogController.java
+++ src/main/java/com/spring/blog/controller/BlogController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -31,5 +32,14 @@ public class BlogController {
         PostDto post = postService.findPostByUrl(postUrl);
         model.addAttribute("post", post);
         return "blog/blog_post";
+    }
+
+    // http://localhost:8080/page/search?query=java
+    @GetMapping("/page/search")
+    public String searchPosts(@RequestParam(value = "query") String query,
+                              Model model){
+        List<PostDto> postsResponse = postService.searchPosts(query);
+        model.addAttribute("postsResponse", postsResponse);
+        return "blog/view_posts";
     }
 }
