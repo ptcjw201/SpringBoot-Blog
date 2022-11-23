912ca58341f39510ec1317981901ce3a09624e0d
ptcjw <ptcjw201@gmail.com>
2022-11-23 12:28:01 +0900

    comment validation 추가

--- src/main/java/com/spring/blog/controller/CommentController.java
+++ src/main/java/com/spring/blog/controller/CommentController.java
@@ -2,27 +2,42 @@ package com.spring.blog.controller;
 
 
 import com.spring.blog.dto.CommentDto;
+import com.spring.blog.dto.PostDto;
 import com.spring.blog.service.CommentService;
 import com.spring.blog.service.PostService;
 import com.spring.blog.service.impl.CommentServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+
 @Controller
 public class CommentController {
     private CommentService commentService;
+    private PostService postService;
 
-    public CommentController(CommentService commentService){
+    public CommentController(CommentService commentService, PostService postService){
         this.commentService = commentService;
+        this.postService = postService;
     }
 
     @PostMapping("/{postUrl}/comments")
     public String createComment(@PathVariable("postUrl") String postUrl,
+                                @Valid
                                 @ModelAttribute("comment") CommentDto commentDto,
+                                BindingResult result,
                                 Model model){
+
+        PostDto postDto = postService.findPostByUrl(postUrl);
+        if(result.hasErrors()){
+            model.addAttribute("post", postDto);
+            model.addAttribute("comment", commentDto);
+            return "blog/blog_post";
+        }
         commentService.createComment(postUrl, commentDto);
         return "redirect:/post/" + postUrl;
 
--- src/main/java/com/spring/blog/dto/CommentDto.java
+++ src/main/java/com/spring/blog/dto/CommentDto.java
@@ -2,6 +2,8 @@ package com.spring.blog.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
 @Data
@@ -10,9 +12,17 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class CommentDto {
     private Long id;
+
+    @NotEmpty
     private String name;
+
+    @NotEmpty(message = "Email should not be empty or null")
+    @Email
     private String email;
+
+    @NotEmpty(message = "Message body should not be empty")
     private String content;
+
     private LocalDateTime createdOn;
     private LocalDateTime modifiedOn;
 }
